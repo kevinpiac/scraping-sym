0b1a7acfcd9cad522fd1614143164744cde976e8
Kevin Piacentini <kevinpiac@gmail.com>
2018-12-11 17:48:47 +0100

    Add csv support

--- src/pyramid_to_csv.js
+++ src/pyramid_to_csv.js
@@ -22,6 +22,7 @@ const _ = require('lodash');
   const arr = await col.find().limit(40).toArray();
   const res = arr.map((elem) => {
     const orignItem = elem;
+    orignItem.categories = orignItem.categories.join(',');
     let newItems = [];
     if (!elem.sessions || !elem.sessions.length) {
       newItems.push(orignItem);
@@ -42,7 +43,65 @@ const _ = require('lodash');
 
   const flat = _.flattenDeep(res);
 
-  flat.forEach((e) => {
-    console.log(`e.begin: ${e.begin}, e.end: ${e.end}, e.url: ${e.url}\n`);
-  });
+  // flat.forEach((e) => {
+  //   console.log(`e.cats: ${e.categories}, e.begin: ${e.begin}, e.end: ${e.end}, e.url: ${e.url}\n`);
+  // });
+
+  /////////// HERE WE GO
+  ///////////
+  ///////////
+  ///////////
+
+  var fs = require("fs")
+  var es = require("event-stream")
+  var jsoncsv = require('json-csv');
+  const items = flat;
+
+  const options = {
+    // begin
+    // end
+    // url,
+    // isCertified,
+    // cpf,
+    // competence_acquises,
+    // sessions,
+    // title,
+    // categories,
+    // level,
+    // duration,
+    // location,
+    // public,
+    // goal,
+    // requirements,
+    // priceHt,
+    // ref,
+    // program,
+    fields: [
+      { label: 'Url', name: 'url' },
+      { label: 'Certifié', name: 'isCertified' },
+      { label: 'CPF', name: 'cpf' },
+      { label: 'Compétences Acquises', name: 'competence_acquises' },
+      { label: 'Titre', name: 'title' },
+      { label: 'Catégories', name: 'categories' },
+      { label: 'Niveau', name: 'level' },
+      { label: 'Durée', name: 'duration' },
+      { label: 'Lieu', name: 'location' },
+      { label: 'Public', name: 'public' },
+      { label: 'Objectifs', name: 'goal' },
+      { label: 'Prérequis', name: 'requirements' },
+      { label: 'Prix HT', name: 'priceHt' },
+      { label: 'Référence', name: 'ref' },
+      { label: 'Programme', name: 'program' },
+      { label: 'Date Début', name: 'begin' },
+      { label: 'Date Fin', name: 'end' },
+    ],
+    fieldSeparator : ';'
+  };
+
+  var out = fs.createWriteStream("pyramyd-output.csv", {encoding: 'utf8'})
+  var readable = es.readArray(items)
+  readable
+    .pipe(jsoncsv.csv(options))
+    .pipe(out)
+
 })();
